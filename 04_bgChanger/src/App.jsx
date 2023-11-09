d878e91a6e2d3a48f3cf146381bf09322acbdd84
abhi-navz <abhinavg01441@gmail.com>
2023-11-09 20:23:02 +0530

    .

--- 04_bgChanger/src/App.jsx
+++ 04_bgChanger/src/App.jsx
@@ -3,7 +3,7 @@ import { useState } from "react";
 function App() {
   const [color, setColor] = useState("olive");
   return (
-    <div className="w-full h-screen " style={{ backgroundColor: color }}>
+    <div className="w-full h-screen duration-200 " style={{ backgroundColor: color }}>
       <div className="fixed bottom-10 flex flex-wrap justify-center inset-x-0 px-2 py-1 mx-3 rounded-full bg-green-300">
         <div className="flex flex-wrap px-2 py-2 bg-white rounded-3xl gap-8">
           <button
