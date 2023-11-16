e03dfd893dea1c60990777f71f4cad93d1d31e3d
abhi-navz <abhinavg01441@gmail.com>
2023-11-16 21:33:01 +0530

    Started yet anotyher project of making Currency converter with React .

---
+++ 06_currencyConverter/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+
+
+function App() {
+  
+
+  return (
+    <>
+      <h1 className='text-3xl text-center bg-blue-600 mx-auto px-auto my-5 rounded-full'>Hello</h1>
+    </>
+  )
+}
+
+export default App
