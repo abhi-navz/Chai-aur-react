f0e8c1304b443a2f9fdea5e9e25bce1dd6a5f188
abhi-navz <abhinavg01441@gmail.com>
2023-11-17 22:55:45 +0530

    fetched datat from api , added functinalities of swap, converting the currency and created the UI

--- 06_currencyConverter/src/hooks/useCurrencyInfo.js
+++ 06_currencyConverter/src/hooks/useCurrencyInfo.js
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 
 function useCurrencyInfo(currency) {
-  let [data, setData] = useState[{}];
+  let [data, setData] = useState({});
   useEffect(() => {
     let url = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`;
     fetch(url)
