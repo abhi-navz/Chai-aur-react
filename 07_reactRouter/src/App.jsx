4f9d44b976f163ee79dc0faaa9fb0d94312b63b4
abhi-navz <abhinavg01441@gmail.com>
2023-11-18 23:18:01 +0530

    Created routes

--- 07_reactRouter/src/App.jsx
+++ 07_reactRouter/src/App.jsx
@@ -1,8 +1,6 @@
 import React from "react";
 
-import Footer from "./Component/Footer/Footer";
-import Header from "./Component/Header/Header";
-import Home from "./Component/Home/Home";
+
 
 function App() {
   return (<>
--- 07_reactRouter/src/Layout.jsx
+++ 07_reactRouter/src/Layout.jsx
@@ -1,16 +1,16 @@
-import React from 'react'
-import Header from './Component/Header/Header'
-import Footer from './Component/Footer/Footer'
-import { Outlet } from 'react-router-dom'
+import React from "react";
+import Header from "./Component/Header/Header";
+import Footer from "./Component/Footer/Footer";
+import { Outlet } from "react-router-dom";
 
 function Layout() {
   return (
-   <>
-   <Header/>
-   <Outlet/>
-   <Footer/>
-   </>
-  )
+    <>
+      <Header />
+      <Outlet />
+      <Footer />
+    </>
+  );
 }
 
-export default Layout
+export default Layout;
