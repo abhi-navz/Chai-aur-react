7479928d8ad9fe8d805b2cf67c735e7327027f67
abhi-navz <abhinavg01441@gmail.com>
2023-11-19 22:19:17 +0530

    Started Learning Context Api

--- 07_reactRouter/src/Layout.jsx
+++ 07_reactRouter/src/Layout.jsx
@@ -7,7 +7,7 @@ function Layout() {
   return (
     <>
       <Header />
-      <Outlet />
+      <Outlet /> // Outlet here is being used in the parent componet Layout to render childrent elements like home,about,contact etc in place out of outlet while keeping the Header and Footer component same.
       <Footer />
     </>
   );
---
+++ 08_miniContextApi/src/App.jsx
@@ -0,0 +1,12 @@
+
+function App() {
+  
+
+  return (
+    <>
+    Context Api
+    </>
+  )
+}
+
+export default App
