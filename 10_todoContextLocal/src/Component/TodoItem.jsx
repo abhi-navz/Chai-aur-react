6744a84f5a259f2bcc526ba95d1f0f350b6f06a5
abhi-navz <abhinavg01441@gmail.com>
2023-12-01 16:42:11 +0530

    Functionality added to TodoItem component.

--- 10_todoContextLocal/src/Component/TodoItem.jsx
+++ 10_todoContextLocal/src/Component/TodoItem.jsx
@@ -1,6 +1,23 @@
-import React from "react";
+import React, { useState } from "react";
+import { useTodo } from "../Context";
 
-function TodoItem() {
+function TodoItem({ todo }) {
+  const [isTodoEditable, setIsTodoEditable] = useState(false);
+  const [todoMsg, setTodoMsg] = useState(todo.todo);
+
+  // importing the requried function from the context!
+  const { updateTodo, deleteTodo, toggleComplete } = useTodo();
+
+  // functionality to edit todo.
+  const editTodo = () => {
+    updateTodo(todo.id, { ...todo, todo: todoMsg });
+    setIsTodoEditable(false);
+  };
+
+  const toggleCompleted = () => {
+    toggleComplete(todo.id);
+  };
+
   return (
     <div
       className={`flex border border-black/10 rounded-lg px-3 py-1.5 gap-x-3 shadow-sm shadow-white/50 duration-300  text-black ${
@@ -36,6 +53,7 @@ function TodoItem() {
       >
         {isTodoEditable ? "📁" : "✏️"}
       </button>
+
       {/* Delete Todo Button */}
       <button
         className="inline-flex w-8 h-8 rounded-lg text-sm border border-black/10 justify-center items-center bg-gray-50 hover:bg-gray-100 shrink-0"
